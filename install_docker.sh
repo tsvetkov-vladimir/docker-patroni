cf7e342c70ab827988f42ac71270736bd471e79d
tsvetkov-vladimir <cvetkov1968@mail.ru>
2021-01-27 21:00:36 +0300

    Update script install docker

--- install_docker.sh
+++ install_docker.sh
@@ -11,3 +11,8 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(
 apt update
 
 apt install docker-ce
+
+curl -L https://github.com/docker/compose/releases/download/1.25.3/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
+chmod +x /usr/local/bin/docker-compose
+
