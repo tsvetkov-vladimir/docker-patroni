07e10001c9f0a1b9e603a46acfa63c81204cacd2
tsvetkov-vladimir <cvetkov1968@mail.ru>
2021-01-28 21:44:19 +0300

    Add script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd etcd/
+docker stack deploy --compose-file docker-compose-cluster.yml patroni
+
+cd ../patroni/
+
+docker stack deploy --compose-file docker-compose-patroni.yml patroni
+
+cd ../haproxy
+
+docker stack deploy --compose-file docker-compose-haproxy.yml patroni
