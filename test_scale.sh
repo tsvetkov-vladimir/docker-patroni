762152b8bd0b74124979c5d15a0091d91abbfa25
tsvetkov-vladimir <cvetkov1968@mail.ru>
2021-02-11 22:57:45 +0300

    Update script

--- test_scale.sh
+++ test_scale.sh
@@ -4,10 +4,16 @@ j=0
 
 while true 
 do
-	i=$(( $j % 3 ))
-	echo $i
+	i=$(( j % 3 + 1))
+	status=$(docker exec -ti $(docker ps -q --filter "name=haproxy") curl -o /dev/null -s -w "%{http_code}\n" http://patroni$i:8091)
+	echo "status patroni$i - $status"
+	if [[ $status = 200 ]]
+	then
+    echo "host patroni$i down..."
+    docker service scale patroni_patroni$i=0
+	fi
 	sleep 1
 	((j++))
 done
 
-docker service scale patroni_patroni3=0
+
