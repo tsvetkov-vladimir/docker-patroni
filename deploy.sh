ecc49de537295dc80e3b823fae4a0c83c530dbf5
tsvetkov-vladimir <cvetkov1968@mail.ru>
2021-03-31 22:41:11 +0300

    Update script

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,18 @@
 cd etcd/
 docker stack deploy --compose-file docker-compose-cluster.yml patroni
 
-#sleep 5
+sleep 5
 
-#cd ../patroni/
+cd ../patroni/
 
-#docker stack deploy --compose-file docker-compose-patroni.yml patroni
+docker stack deploy --compose-file docker-compose-patroni.yml patroni
 
-#sleep 15
+sleep 15
 
-#cd ../haproxy/
+cd ../haproxy/
 
-#docker stack deploy --compose-file docker-compose-haproxy.yml patroni
+docker stack deploy --compose-file docker-compose-haproxy.yml patroni
 
-#sleep 10
+sleep 10
 
-#docker service ls
+docker service ls
