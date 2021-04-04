6cfa9f8331c512dac367b3863baaeb7a0c6a68e6
tsvetkov-vladimir <cvetkov1968@mail.ru>
2021-04-04 23:42:14 +0300

    Update script

--- test_scale.sh
+++ test_scale.sh
@@ -6,7 +6,7 @@ k=0
 while true 
 do
 	i=$(( j % 3 + 1))
-	status=$(docker exec -ti "$(docker ps -q --filter "name=haproxy")" curl -o /dev/null -s -w "%{http_code}\n" http://patroni$i:8091)
+	status=$(docker exec -ti "$(docker ps -q | head -n 1)" curl -o /dev/null -s -w "%{http_code}\n" http://patroni$i:8091)
 	echo "status patroni$i - ${status}"
 	status="$(echo -e "${status}" | tr -d '[:space:]')"
 	if [[ ${status} = 200 ]]
