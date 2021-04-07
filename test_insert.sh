2db0bbf81f849675a00b7bcf36bd204d010fe842
tsvetkov-vladimir <cvetkov1968@mail.ru>
2021-04-07 23:17:52 +0300

    Update script

--- test_insert.sh
+++ test_insert.sh
@@ -2,5 +2,5 @@
 
 while true
 do
-  psql postgresql://postgres:supass@127.0.0.1:5000/test -c "INSERT INTO test VALUES (DEFAULT)" > /dev/null
+  psql postgresql://postgres:supass@127.0.0.1:5000/test -c "INSERT INTO test VALUES (DEFAULT)" > /dev/null 2>&1
 done
